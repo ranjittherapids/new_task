6cb092565da2c7dc6371240b6a7f3f5e8fb6036d
Ranjit <rapidskumar@gmail.com>
2021-12-16 09:54:01 +0530

    initilaiase project babay

---
+++ src/component/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './layout.css'
+export default function layout() {
+    return (
+        <div className='layoutcontainer'> 
+          <div className='sidebarcontainer'></div>
+          <div className='bodypartcontainer'></div>
+        </div>
+    )
+}
